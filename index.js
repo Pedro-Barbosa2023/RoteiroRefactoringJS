2b23c1cb32ec91d975e2dc91ac29754139c04cc4
Pedro Barbosa <pedro.barbosa314150@gmail.com>
2023-11-15 23:05:12 -0300

    Commit 8 - Classe Repositorio

--- index.js
+++ index.js
@@ -1,50 +1,39 @@
 const { readFileSync } = require('fs');
 
-// Função extraída
-function formatarMoeda(valor) {
-  return new Intl.NumberFormat("pt-BR",
-    { style: "currency", currency: "BRL",
-      minimumFractionDigits: 2 }).format(valor/100);
-}
-
-// Função extraída
-function getPeca(pecas, apresentacao) {
-  return pecas[apresentacao.id];
-}
-
-// Função para gerar fatura em formato de string
-function gerarFaturaStr(fatura, pecas, servicoCalculo) {
-  let faturaStr = `Fatura ${fatura.cliente}\n`;
-  for (let apre of fatura.apresentacoes) {
-    faturaStr += `  ${getPeca(pecas, apre).nome}: ${formatarMoeda(servicoCalculo.calcularTotalApresentacao(pecas, apre))} (${apre.audiencia} assentos)\n`;
+class Repositorio {
+  constructor() {
+    this.pecas = JSON.parse(readFileSync('./pecas.json'));
   }
-  faturaStr += `Valor total: ${formatarMoeda(servicoCalculo.calcularTotalFatura(pecas, fatura.apresentacoes))}\n`;
-  faturaStr += `Créditos acumulados: ${servicoCalculo.calcularTotalCreditos(pecas, fatura.apresentacoes)} \n`;
-  return faturaStr;
+
+  getPeca(apre) {
+    return this.pecas[apre.id];
+  }
 }
 
-// Classe para cálculos de fatura
 class ServicoCalculoFatura {
+  constructor(repo) {
+    this.repo = repo;
+  }
 
-  calcularCredito(pecas, apre) {
+  calcularCredito(apre) {
     let creditos = 0;
     creditos += Math.max(apre.audiencia - 30, 0);
-    if (getPeca(pecas, apre).tipo === "comedia") 
+    if (this.repo.getPeca(apre).tipo === "comedia") 
       creditos += Math.floor(apre.audiencia / 5);
     return creditos;   
   }
 
-  calcularTotalCreditos(pecas, apresentacoes) {
+  calcularTotalCreditos(apresentacoes) {
     let creditos = 0;
     for (let apre of apresentacoes) {
-      creditos += this.calcularCredito(pecas, apre);
+      creditos += this.calcularCredito(apre);
     }
     return creditos;
   }
 
-  calcularTotalApresentacao(pecas, apre) {
+  calcularTotalApresentacao(apre) {
     let total = 0;
-    const peca = getPeca(pecas, apre);
+    const peca = this.repo.getPeca(apre);
 
     switch (peca.tipo) {
       case "tragedia":
@@ -66,45 +55,34 @@ class ServicoCalculoFatura {
     return total;
   }
 
-  calcularTotalFatura(pecas, apresentacoes) {
+  calcularTotalFatura(apresentacoes) {
     let totalFatura = 0;
     for (let apre of apresentacoes) {
-      totalFatura += this.calcularTotalApresentacao(pecas, apre);
+      totalFatura += this.calcularTotalApresentacao(apre);
     }
     return totalFatura;
   }
 }
 
-const faturas = JSON.parse(readFileSync('./faturas.json'));
-const pecas = JSON.parse(readFileSync('./pecas.json'));
-
-// Criar objeto da nova classe
-const calc = new ServicoCalculoFatura();
-
-// Gerar e exibir fatura em formato de string
-const faturaStr = gerarFaturaStr(faturas, pecas, calc);
-console.log("Fatura em String:\n", faturaStr);
-
-// Comente o corpo da função gerarFaturaHTML e sua chamada para ter apenas uma saída
-/*
-// Função para gerar fatura em formato HTML
-function gerarFaturaHTML(fatura, pecas) {
-  let faturaHTML = `<html>\n<p> Fatura ${fatura.cliente} </p>\n<ul>\n`;
-
+// Removendo o parâmetro pecas de gerarFaturaStr
+function gerarFaturaStr(fatura, servicoCalculo) {
+  let faturaStr = `Fatura ${fatura.cliente}\n`;
   for (let apre of fatura.apresentacoes) {
-    const peca = getPeca(pecas, apre);
-    const total = calc.calcularTotalApresentacao(pecas, apre);
-    faturaHTML += `<li>  ${peca.nome}: ${formatarMoeda(total)} (${apre.audiencia} assentos) </li>\n`;
+    faturaStr += `  ${servicoCalculo.repo.getPeca(apre).nome}: ${formatarMoeda(servicoCalculo.calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)\n`;
   }
-
-  const totalFatura = calc.calcularTotalFatura(pecas, fatura.apresentacoes);
-  const totalCreditos = calc.calcularTotalCreditos(pecas, fatura.apresentacoes);
-
-  faturaHTML += `</ul>\n<p> Valor total: ${formatarMoeda(totalFatura)} </p>\n<p> Créditos acumulados: ${totalCreditos} </p>\n</html>`;
-  return faturaHTML;
+  faturaStr += `Valor total: ${formatarMoeda(servicoCalculo.calcularTotalFatura(fatura.apresentacoes))}\n`;
+  faturaStr += `Créditos acumulados: ${servicoCalculo.calcularTotalCreditos(fatura.apresentacoes)} \n`;
+  return faturaStr;
 }
 
-// Gerar e exibir fatura em formato HTML
-const faturaHTML = gerarFaturaHTML(faturas, pecas);
-console.log("\nFatura em HTML:\n", faturaHTML);
-*/
+// Função extraída
+function formatarMoeda(valor) {
+  return new Intl.NumberFormat("pt-BR",
+    { style: "currency", currency: "BRL",
+      minimumFractionDigits: 2 }).format(valor/100);
+}
+
+const faturas = JSON.parse(readFileSync('./faturas.json'));
+const calc = new ServicoCalculoFatura(new Repositorio());
+const faturaStr = gerarFaturaStr(faturas, calc);
+console.log(faturaStr);
